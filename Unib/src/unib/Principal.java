01a3705bf5b598e82972719122d7446fa6427bac
Beatriz Almeida <BeaAlmeida@users.noreply.github.com>
2020-10-17 02:40:37 -0300

    17/10
    telas quase prontas no netbeans

---
+++ Unib/src/unib/Principal.java
@@ -0,0 +1,14 @@
+package unib;
+
+import view.TelaInicio;
+
+public class Principal {
+
+    public static void main(String[] args) {
+        
+        TelaInicio ti = new TelaInicio();
+        ti.setVisible(true);
+        
+    }
+    
+}
