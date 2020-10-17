1b04ea16e81e38be6ca4a9c87990ae5595f58fff
Bot-Mateus <63660899+Bot-Mateus@users.noreply.github.com>
2020-10-17 16:33:43 -0300

    Add files via upload

---
+++ Unib 17-10/src/util/Mascara.java
@@ -0,0 +1,57 @@
+
+package util;
+
+import java.math.RoundingMode;
+import java.text.DecimalFormat;
+import java.text.ParseException;
+import javax.swing.text.DefaultFormatterFactory;
+import javax.swing.text.MaskFormatter;
+import javax.swing.text.NumberFormatter;
+
+
+public class Mascara {
+    public static DefaultFormatterFactory getCpfMask(){
+        MaskFormatter mask = null;
+        try{
+            mask = new MaskFormatter("###.###.###-##");
+            mask.setPlaceholderCharacter('_');
+        }catch (ParseException ex){
+            return null;
+        }
+        return(new DefaultFormatterFactory (mask, mask));
+    }
+    
+    public static DefaultFormatterFactory getFoneFixoMask(){
+        MaskFormatter mask = null;
+        try{
+            mask = new MaskFormatter("(##) ####-####");
+            mask.setPlaceholderCharacter('_');
+        }catch (ParseException ex){
+            return null;
+        }
+        return(new DefaultFormatterFactory (mask, mask));
+    }
+    
+    public static DefaultFormatterFactory getCelularMask(){
+        MaskFormatter mask = null;
+        try{
+            mask = new MaskFormatter("(##) #####-####");
+            mask.setPlaceholderCharacter('_');
+        }catch (ParseException ex){
+            return null;
+        }
+        return(new DefaultFormatterFactory (mask, mask));
+    }
+    
+    public static DefaultFormatterFactory getDataNascMask(){
+        MaskFormatter mask = null;
+        try{
+            mask = new MaskFormatter("##/##/####");
+            mask.setPlaceholderCharacter('_');
+        }catch (ParseException ex){
+            return null;
+        }
+        return(new DefaultFormatterFactory (mask, mask));
+    }
+    
+}
